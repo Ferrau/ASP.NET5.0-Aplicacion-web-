227c2b4101d82f721993774840433e360a59e0a4
Ester Ferrau <esterferrau@gmail.com>
2022-05-21 00:02:19 -0300

    20052022 Ultimos cambios desde la clase 1 a la clase el 9

---
+++ MvcMovie/Data/SeedData.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using MvcMovie.Data;
+
+using System;
+
+using System.Linq;
+
+
+namespace MvcMovie.Models
+
+{
+    /*
+     * Inicializar la base de datos
+     */
+
+    public static class SeedData
+
+    {
+
+        public static void Initialize(IServiceProvider serviceProvider)
+
+        {
+
+            using (var context = new MvcMovieContext(
+
+                serviceProvider.GetRequiredService<
+
+                    DbContextOptions<MvcMovieContext>>()))
+
+            {
+
+                // Look for any movies.
+                //Si hay alguna película en la base de datos, se devuelve el inicializador y no se agrega ninguna película.
+
+                
+
+                if (context.Movie.Any())
+
+                {
+
+                    return;   // DB has been seeded
+
+                }
+
+
+                context.Movie.AddRange(
+
+                    new Movie
+
+                    {
+
+                        Title = "When Harry Met Sally",
+
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+
+                        Genre = "Romantic Comedy",
+
+                        Rating = "R",
+
+                        Price = 7.99M
+
+                    },
+
+
+                    new Movie
+
+                    {
+
+                        Title = "Ghostbusters ",
+
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+
+                        Genre = "Comedy",
+                        Rating = "R",
+
+                        Price = 8.99M
+
+                    },
+
+
+                    new Movie
+
+                    {
+
+                        Title = "Ghostbusters 2",
+
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+
+                        Genre = "Comedy",
+
+                        Rating = "R",
+
+                        Price = 9.99M
+
+                    },
+
+
+                    new Movie
+
+                    {
+
+                        Title = "Rio Bravo",
+
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+
+                        Genre = "Western",
+
+                        Rating = "R",
+
+                        Price = 3.99M
+
+                    }
+
+                );
+
+                context.SaveChanges();
+
+            }
+
+        }
+
+    }
+
+}
---
+++ MvcMovie/Models/MovieGenreViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+using System.Collections.Generic;
+
+
+namespace MvcMovie.Models
+
+{
+    /*Adición de búsqueda por género*/
+    public class MovieGenreViewModel
+
+    {
+
+        public List<Movie> Movies { get; set; }
+
+        public SelectList Genres { get; set; }
+
+        public string MovieGenre { get; set; }
+
+        public string SearchString { get; set; }
+
+    }
+
+}
